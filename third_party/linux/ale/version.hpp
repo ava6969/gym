bcffb4e6b5b5a5e4b7f75fcbbb2e630f6c4a2427
dewe <ava6969@rit.edu>
2022-03-01 16:28:18 -0500

    gym static builds added

---
+++ third_party/linux/ale/version.hpp
@@ -0,0 +1,14 @@
+#ifndef __VERSION_HPP__
+#define __VERSION_HPP__
+
+#define ALE_VERSION "0.6.2"
+#define ALE_VERSION_MAJOR "0"
+#define ALE_VERSION_MINOR "6"
+#define ALE_VERSION_PATCH "2"
+#define ALE_VERSION_GIT_SHA "e016d07"
+
+// This isn't entirely accurate, there's been
+// some changes post 2.4.2.
+#define STELLA_VERSION "2.4.2"
+
+#endif // __VERSION_HPP__
