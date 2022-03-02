3290565aec26d683464d4bb969e8ac4d815a1b1b
dewe <ava6969@rit.edu>
2022-03-02 11:18:06 -0500

    replaced gym

---
+++ third_party/ale/include/ale/version.hpp
@@ -0,0 +1,14 @@
+#ifndef __VERSION_HPP__
+#define __VERSION_HPP__
+
+#define ALE_VERSION "0.7.3"
+#define ALE_VERSION_MAJOR "0"
+#define ALE_VERSION_MINOR "7"
+#define ALE_VERSION_PATCH "3"
+#define ALE_VERSION_GIT_SHA "0af0ff4"
+
+// This isn't entirely accurate, there's been
+// some changes post 2.4.2.
+#define STELLA_VERSION "2.4.2"
+
+#endif // __VERSION_HPP__
